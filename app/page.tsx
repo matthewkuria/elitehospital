9650dceb3526d888b342692e613d08a818d4b982
Matthewkuria <mathewkwachira@gmail.com>
2025-10-24 16:28:26 +0300

    Remove the duplicate header from the page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-import Image from "next/image";
 import Hero from "./components/Hero";
-import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col">
-      <NavBar />
       <Hero />
     </main>
   );
